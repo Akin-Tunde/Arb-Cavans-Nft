140e09c7a561d32b5d1b3afa30af5932fe846937
Akin-Tunde <wang23hao@gmail.com>
2025-08-22 16:05:48 +0100

    Update PixelNFT.sol

--- contracts/PixelNFT.sol
+++ contracts/PixelNFT.sol
@@ -3,56 +3,70 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-/**
- * @title PixelNFT
- * @author Your Name
- * @notice This ERC-721 contract is the secure "Deed Office" for the Farcaster Canvas.
- * It manages the ownership and color of individual pixel NFTs.
- * @dev Its owner is the FarcasterCanvas contract, which has exclusive minting rights.
- * This separation of concerns is a key part of the security model.
- */
 contract PixelNFT is ERC721, Ownable {
     uint256 public immutable canvasWidth;
-
     mapping(uint256 => uint8) public pixelColors;
 
     event ColorChanged(uint256 indexed tokenId, uint8 newColorIndex);
 
-    constructor(uint256 _canvasWidth, address initialOwner) 
-        ERC721("Farcaster Pixel", "PIXEL") 
+    constructor(string memory _name, string memory _symbol, uint256 _canvasWidth, address initialOwner) 
+        ERC721(_name, _symbol) 
         Ownable(initialOwner) 
     {
         canvasWidth = _canvasWidth;
     }
 
-    /**
-     * @notice Mints a new pixel NFT.
-     * @dev This function is protected by `onlyOwner`. Only the FarcasterCanvas contract,
-     * which is set as the owner during deployment, can ever call this function.
-     * This prevents any unauthorized creation of pixel NFTs.
-     * @param to The address to receive the new NFT.
-     * @param tokenId The unique ID of the pixel to be minted.
-     * @param colorIndex The initial color of the pixel.
-     */
     function mint(address to, uint256 tokenId, uint8 colorIndex) public onlyOwner {
         _safeMint(to, tokenId);
         pixelColors[tokenId] = colorIndex;
     }
 
-    /**
-     * @notice Allows the owner of a pixel NFT to change its color.
-     * @dev The check `ownerOf(tokenId) == msg.sender` is the modern, secure way to
-     * ensure that only the rightful owner of the NFT can modify its state.
-     * @param tokenId The ID of the pixel to change.
-     * @param newColorIndex The new color index to set for the pixel.
-     */
     function changeColor(uint256 tokenId, uint8 newColorIndex) public {
-        // --- THIS IS THE CORRECTED LINE ---
-        // It fetches the owner of the token and requires it to be the person calling the function.
         require(ownerOf(tokenId) == msg.sender, "PixelNFT: Caller is not the owner");
-        
         pixelColors[tokenId] = newColorIndex;
         emit ColorChanged(tokenId, newColorIndex);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        uint8 colorIndex = pixelColors[tokenId];
+        string memory colorHex = _getHexColor(colorIndex);
+
+        string memory svgImage = string(abi.encodePacked(
+            '<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">',
+            '<rect width="100%" height="100%" fill="#', colorHex, '" />',
+            '</svg>'
+        ));
+
+        string memory json = string(abi.encodePacked(
+            '{',
+                '"name": "', name(), ' #', Strings.toString(tokenId), '",',
+                '"description": "A single pixel on a collaborative, on-chain canvas.",',
+                '"image": "data:image/svg+xml;base64,', Base64.encode(bytes(svgImage)), '",',
+                '"attributes": [',
+                    '{',
+                        '"trait_type": "Color Index",',
+                        '"value": ', Strings.toString(colorIndex),
+                    '}',
+                ']',
+            '}'
+        ));
+        
+        return string(abi.encodePacked(
+            "data:application/json;base64,",
+            Base64.encode(bytes(json))
+        ));
+    }
+
+    function _getHexColor(uint8 colorIndex) internal pure returns (string memory) {
+        if (colorIndex == 1) return "FF0000"; if (colorIndex == 2) return "FFFF00"; if (colorIndex == 3) return "0000FF";
+        if (colorIndex == 4) return "FFA500"; if (colorIndex == 5) return "008000"; if (colorIndex == 6) return "8A2BE2";
+        if (colorIndex == 7) return "FFC0CB"; if (colorIndex == 8) return "A52A2A"; if (colorIndex == 9) return "FFD700";
+        if (colorIndex == 10) return "800080"; if (colorIndex == 11) return "00FFFF"; if (colorIndex == 12) return "FF00FF";
+        if (colorIndex == 13) return "FFFFFF"; if (colorIndex == 14) return "D3D3D3"; if (colorIndex == 15) return "808080";
+        if (colorIndex == 16) return "363636"; if (colorIndex == 17) return "C0C0C0"; if (colorIndex == 18) return "000000";
+        return "CCCCCC";
     }
 }
